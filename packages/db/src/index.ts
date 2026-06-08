df6aa08ade714a45edaa4526f2bd918e6130d588
ankitR3 <ankitraj828401@gmail.com>
2026-06-08 16:22:08 +0530

    removed from db

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,7 +3,6 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
